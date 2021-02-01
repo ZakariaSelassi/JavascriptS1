40789463caebb1937af64f413b41533240e1f694
zakaria <zakariaselassi@gmail.com>
2021-02-01 13:33:48 +0100

    calculator-two

--- 02-maths/02-calculator-two/script.js
+++ 02-maths/02-calculator-two/script.js
@@ -1,4 +1,3 @@
-
 // 02-maths/02-calculator-two/script.js - 2.2: calculatrice (2)
 
 (() => {
@@ -6,12 +5,39 @@
 
     const performOperation = operation => {
         // perform the operation
+        let one = document.getElementById("op-one").value;
+        let two = document.getElementById("op-two").value;
+        switch (operation) {
+            case 'addition':
+                let sum = one + two;
+                alert("result :" + sum);
+                break;
+            case 'substraction':
+                let sub = one - two;
+                alert("result :" + sub);
+                break;
+                division
+            case 'multiplication':
+                let mult = one * two;
+                alert("result :" + mult);
+                break;
+            case 'division':
+                let div = one / two;
+                alert("result :" + div);
+                break;
+            default:
+                alert("something wrong!");
+                break;
+        }
     };
 
     Array.from(document.querySelectorAll("button.operator")).forEach($btn =>
         $btn.addEventListener(
             "click",
             () => (performOperation($btn.id), false),
+            console.log(Array)
         ),
+
     );
+
 })();
