cf1b8208a591837b0ac77541ce7e1f7710c3081f
zakaria <zakariaselassi@gmail.com>
2021-02-01 16:57:19 +0100

    title-by-hours

--- 04-dates/01-title-by-hour-one/script.js
+++ 04-dates/01-title-by-hour-one/script.js
@@ -1,4 +1,3 @@
-
 // 04-dates/01-title-by-hour-one/script.js - 4.1: text according to the hour (1)
 
 
@@ -7,5 +6,12 @@
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
+    let endDay = new Date();
+    endDay.setDate(18, 0, 0);
+    if (endDay <= 18) {
+        document.getElementById("target").innerHTML = "Good Evening";
+    } else {
+        document.getElementById("target").innerHTML = "Hello";
+    }
 
 })();
