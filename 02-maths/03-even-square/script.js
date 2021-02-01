fd351af193673ed444434c0a46d1fa99c0abf4fd
zakaria <zakariaselassi@gmail.com>
2021-02-01 13:51:46 +0100

    even square

--- 02-maths/03-even-square/script.js
+++ 02-maths/03-even-square/script.js
@@ -1,12 +1,14 @@
-
 // 02-maths/03-even-square/script.js - 2.3: squares and pairs
 
 
 (() => {
 
     document.getElementById("run").addEventListener("click", () => {
+        let a = 0;
+        for (let i = 0; i <= 21; i++) {
+            console.log(Math.sqrt(i));
 
-        // your code here
+        }
 
     });
 
