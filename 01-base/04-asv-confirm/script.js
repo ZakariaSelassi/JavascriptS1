d32e4fc7ec96fe474b2374d9b8c9a1427305b88b
zakaria <zakariaselassi@gmail.com>
2021-02-01 11:04:39 +0100

    asv-confirm ok

--- 01-base/04-asv-confirm/script.js
+++ 01-base/04-asv-confirm/script.js
@@ -1,10 +1,18 @@
-
 // 01-base/04-asv-confirm/script.js - 1.4: ASV avec confirmation
 
-(() => {
-    // Don't bother with that, look at your browser's console(chrome), you can delete it afterwards ;)
-    console.log('%c ', 'padding: 38px; background:url(https://becode.org/app/uploads/2020/03/bc_mailsign_seal.png) no-repeat;');
-    console.log('Hello Woods programmer');
+let myAge = prompt("Enter your age ? ", "23 year old");
+let myGender = prompt("Enter your gendenr ", "Men or Women");
 
-    // YOUR CODE HERE
-})();
+let myTown = prompt("Enter your town", "City");
+
+alert("Your information : " + myAge + " " + myGender + " " + myTown);
+if (confirm("Do you wants to confirme ?")) {
+    alert("You have confirmed");
+} else {
+
+    myAge = prompt("Enter your age ? ", "23 year old");
+    myGender = prompt("Enter your gendenr ", "Men or Women");
+
+    myTown = prompt("Enter your town", "City");
+
+}
