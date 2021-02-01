03aa1c2929985e2db20b63eb4b7dc74f6f807ea9
zakaria <zakariaselassi@gmail.com>
2021-02-01 16:08:20 +0100

    change-bcg-one

--- 03-colors/01-change-bcg-one/script.js
+++ 03-colors/01-change-bcg-one/script.js
@@ -1,9 +1,12 @@
-
 // 03-colors/01-change-bcg-one/script.js - 3.1: Bcg one (1)
 
 
 (() => {
 
     // your code here
+    document.getElementById("red").addEventListener("click", () => document.body.style.backgroundColor = "Red");
+    document.getElementById("blue").addEventListener("click", () => document.body.style.backgroundColor = "blue");
+    document.getElementById("yellow").addEventListener("click", () => document.body.style.backgroundColor = "yellow");
+    document.getElementById("green").addEventListener("click", () => document.body.style.backgroundColor = "green");
 
 })();
