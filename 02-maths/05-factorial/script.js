3748717dc37a5909dd104ae6cd74b55cac158551
zakaria <zakariaselassi@gmail.com>
2021-02-01 14:46:32 +0100

    factorial

--- 02-maths/05-factorial/script.js
+++ 02-maths/05-factorial/script.js
@@ -1,13 +1,21 @@
-
 // 02-maths/05-factorial/script.js - 2.5: Factorial
 
 (() => {
-    
+
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("run").addEventListener("click", () => {
 
         // your code here
+        let num = document.getElementById("number").value;
+
+        if (num < 0) {
+            return 0;
+        } else if (num == 0) {
+            return 1;
+        } else {
+            return alert(num * (num - 1));
+        }
 
     });
 
