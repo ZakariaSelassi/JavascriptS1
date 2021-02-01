3a2346a29922bae4f4b6f752221330bb4d3acdd0
zakaria <zakariaselassi@gmail.com>
2021-02-01 10:38:17 +0100

    Prompt if

--- 01-base/03-prompt-if/script.js
+++ 01-base/03-prompt-if/script.js
@@ -1,11 +1,9 @@
-
 // 01-base/03-prompt-if/script.js - 1.3: promp & if
 
-(() => {
-  // Don't bother with that, look at your browser's console(chrome), you can delete it afterwards ;)
-  console.log('%c ', 'padding: 38px; background:url(https://becode.org/app/uploads/2020/03/bc_mailsign_seal.png) no-repeat;');
-  console.log('Hello Woods programmer');
+let myVar = prompt("Do you wants some cake ?", "Yes or No ?");
 
-  // YOUR CODE HERE
-})();
-
+if (myVar === "Yes") {
+    alert("Congratulation !");
+} else {
+    alert("more cake for me then :p");
+}
