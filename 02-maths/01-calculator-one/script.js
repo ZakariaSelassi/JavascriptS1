ee1e7ce9df507457effe7ba8c0ea808e598729fe
zakaria <zakariaselassi@gmail.com>
2021-02-01 11:28:50 +0100

    calcuator-one

--- 02-maths/01-calculator-one/script.js
+++ 02-maths/01-calculator-one/script.js
@@ -1,4 +1,3 @@
-
 // 02-maths/01-calculator-one/script.js - 2.1: calculator1
 
 (() => {
@@ -6,17 +5,32 @@
 
     document.getElementById("addition").addEventListener("click", function() {
         // perform an addition
+
+        let one = document.getElementById("op-one").value;
+        let two = document.getElementById("op-two").value;
+        let sum = one + two;
+        return alert("le resultat : " + sum);
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
-        // perform an substraction
+        let one = document.getElementById("op-one").value;
+        let two = document.getElementById("op-two").value;
+        let sub = one - two;
+        return alert("le resultat : " + sub);
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
-        // perform an multiplication
+        let one = document.getElementById("op-one").value;
+        let two = document.getElementById("op-two").value;
+        let mul = one * two;
+        return alert("le resultat : " + mul);
     });
 
     document.getElementById("division").addEventListener("click", function() {
         // perform an division
+        let one = document.getElementById("op-one").value;
+        let two = document.getElementById("op-two").value;
+        let div = one / two;
+        return alert("le resultat : " + div);
     });
 })();
