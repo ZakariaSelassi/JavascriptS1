eebd7a8fddd0839bf447c5c214159d7120bbf119
zakaria <zakariaselassi@gmail.com>
2021-02-01 15:08:21 +0100

    fizzbuzz

--- 02-maths/06-fizzbuzz/script.js
+++ 02-maths/06-fizzbuzz/script.js
@@ -1,8 +1,18 @@
-
 // 02-maths/06-fizzbuzz/script.js - 2.6: fizzbuzz
 
 (() => {
 
+    let n;
     // your code here
+    for (let i = 0; i < 100; i++) {
+        n = Math.floor(Math.random() * (100 - 1) + 1);
+        if (n % 3) {
+            console.log("fizz" + n);
+        } else if (n % 5) {
+            console.log("buzz" + n);
+        } else if (n % 3 == 0 && n % 5 == 0) {
+            console.log("fizzbuzz" + n);
+        }
+    }
 
 })();
