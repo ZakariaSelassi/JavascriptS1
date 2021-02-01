d69975ac211287a1ddc7940c10189b143d0e0bbb
zakaria <zakariaselassi@gmail.com>
2021-02-01 16:26:25 +0100

    change-bcg-two

--- 03-colors/02-change-bcg-two/script.js
+++ 03-colors/02-change-bcg-two/script.js
@@ -1,8 +1,13 @@
-
 // 03-colors/02-change-bcg-two/script.js - 3.2: Bcg 2
 
 (() => {
 
     // your code here
 
+    document.getElementById("run").addEventListener("click", function() {
+        // perform an division
+        let myColor = document.getElementById("color").value;
+        document.body.style.backgroundColor = myColor;
+        return console.log(myColor);
+    });
 })();
