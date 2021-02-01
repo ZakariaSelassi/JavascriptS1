7141ac41869609242e9084ac1c81f515acea7be1
zakaria <zakariaselassi@gmail.com>
2021-02-01 16:43:07 +0100

    change-bcg-three

--- 03-colors/03-change-bcg-three/script.js
+++ 03-colors/03-change-bcg-three/script.js
@@ -1,9 +1,17 @@
-
 // 03-colors/03-change-bcg-three/script.js - 3.3: background color (3)
 
 
 (() => {
 
     // your code here
+    document.getElementById("run").addEventListener("click", function() {
+        // perform an division
+        let letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color = color + letters[Math.floor(Math.random() * 16)];
+        }
+        document.body.style.backgroundColor = color;
+    });
 
 })();
