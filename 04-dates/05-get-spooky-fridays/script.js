9d5403cff90e7feb10f03d5c702d4aa78116bb2c
zakaria <zakariaselassi@gmail.com>
2021-02-02 11:19:47 +0100

    a finir

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -1,9 +1,15 @@
-
 // 04-dates/05-get-spooky-fridays/script.js - 4.5: calcul des vendredi 13
 
 
 (() => {
 
     // your code here
+    document.getElementById("run").addEventListener("click", function() {
+        let y = new Date();
+        annee = y.getFullYear();
+        console.log(annee);
+        document.getElementById("year").value;
 
+        console.log(annee);
+    });
 })();
