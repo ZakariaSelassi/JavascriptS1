fdaa17f58f1b026e4ac0a199fa73eb0232646651
zakaria <zakariaselassi@gmail.com>
2021-02-02 16:26:44 +0100

    rand-array-stats

--- 05-arrays/09-rand-array-stats/script.js
+++ 05-arrays/09-rand-array-stats/script.js
@@ -2,6 +2,29 @@
 
 (() => {
 
-    // your code here
 
+
+    // your code here
+    document.getElementById("run").addEventListener("click", () => MyArray());
+
+    function MyArray() {
+        let n;
+        let stock = [];
+        let sum = 0;
+        let average = 0;
+        let array = document.querySelectorAll("table tr td");
+        for (let i = 0; i < array.length; i++) {
+            n = Math.floor(Math.random() * (100 - 1) + 1);
+            array[i].innerHTML = n;
+            sum += n;
+            average = sum / array.length;
+            stock.push(n);
+        }
+        console.log(stock);
+
+        document.getElementById("min").innerHTML = Math.min(...stock);
+        document.getElementById("max").innerHTML = Math.max(...stock);
+        document.getElementById("sum").innerHTML = sum;
+        document.getElementById("average").innerHTML = average;
+    }
 })();
