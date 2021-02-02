9427606dec5dfdb89d50ad9e1fc50e9a7472cf56
zakaria <zakariaselassi@gmail.com>
2021-02-02 12:20:30 +0100

    walk-two

--- 05-arrays/04-walk-two/script.js
+++ 05-arrays/04-walk-two/script.js
@@ -19,5 +19,9 @@
     ];
 
     // your code here
+    document.getElementById("run").addEventListener("click", () => MyArray(fruits));
 
+    function MyArray() {
+        fruits.forEach(e => console.log(e));
+    }
 })();
