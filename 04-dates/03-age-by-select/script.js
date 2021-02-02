f7c831911cb2585a6f3a3a5c47090f56d1bb2ab4
zakaria <zakariaselassi@gmail.com>
2021-02-02 09:59:02 +0100

    age-by-select

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -1,9 +1,18 @@
-
 // 04-dates/03-age-by-select/script.js - 4.3: calculate the age
 
 
 (() => {
 
     // your code here
+    document.getElementById("run").addEventListener("click", function() {
+
+        let day = document.getElementById("dob-day").value;
+        let month = document.getElementById("dob-month").value;
+        let year = document.getElementById("dob-year").value;
+        let birthDay = new Date(year, month - 1, day);
+        let now = new Date();
+        let age = now.getFullYear() - birthDay.getFullYear();
+        return console.log(birthDay.toDateString() + " age : " + age--);
+    });
 
 })();
