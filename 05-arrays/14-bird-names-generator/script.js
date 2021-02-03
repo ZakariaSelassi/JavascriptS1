e2b2602ce7d2e3e09a997ef4d5fc6c661e5f5272
zakaria <zakariaselassi@gmail.com>
2021-02-03 14:11:07 +0100

    birds

--- 05-arrays/14-bird-names-generator/script.js
+++ 05-arrays/14-bird-names-generator/script.js
@@ -3,18 +3,18 @@
 
 (() => {
     const birds = [
-        {name: "mouette", fem: true},
-        {name: "corbeau"},
-        {name: "mésange", fem: true},
-        {name: "hibou"},
-        {name: "buse", fem: true},
-        {name: "pigeon"},
-        {name: "pie", fem: true},
-        {name: "vautour"},
-        {name: "faucon"},
-        {name: "rouge-gorge"},
-        {name: "tourterelle", fem: true},
-        {name: "corneille", fem: true},
+        { name: "mouette", fem: true },
+        { name: "corbeau" },
+        { name: "mésange", fem: true },
+        { name: "hibou" },
+        { name: "buse", fem: true },
+        { name: "pigeon" },
+        { name: "pie", fem: true },
+        { name: "vautour" },
+        { name: "faucon" },
+        { name: "rouge-gorge" },
+        { name: "tourterelle", fem: true },
+        { name: "corneille", fem: true },
     ];
     const adjectives = new Set([
         "cendré",
@@ -31,4 +31,19 @@
     ]);
 
     // your code here
+
+    document.getElementById("run").addEventListener("click", () => myFunction());
+
+    function myFunction() {
+        let n;
+        let array = [...adjectives];
+
+        n = birds[Math.floor(Math.random() * birds.length)];
+        array = array[Math.floor(Math.random() * array.length)];
+        console.log(n);
+        console.log(array);
+
+        document.getElementById("target").innerHTML = n.name + " " + array;
+
+    }
 })();
