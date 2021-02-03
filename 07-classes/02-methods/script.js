208070fad42357739e2274261d0e593f61768736
zakaria <zakariaselassi@gmail.com>
2021-02-03 21:22:00 +0100

    methods

--- 07-classes/02-methods/script.js
+++ 07-classes/02-methods/script.js
@@ -3,4 +3,22 @@
 
 (() => {
     // your code here
+    class Person {
+        constructor(firstname, lastname) {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+        sayHello() {
+            return console.log("Hello " + this.firstname + " " + this.lastname);
+        }
+    }
+    document.getElementById("run").addEventListener("click", () => myFunction());
+
+    function myFunction() {
+
+        let myPerson = new Person("Zakaria", "Selassi");
+        myPerson.sayHello();
+    }
+
 })();
