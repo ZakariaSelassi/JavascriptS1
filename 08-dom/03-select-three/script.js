7f18f02ba28e6615b7ecf420fb9a8ef38b84361f
zakaria <zakariaselassi@gmail.com>
2021-02-04 11:54:56 +0100

    select-three

--- 08-dom/03-select-three/script.js
+++ 08-dom/03-select-three/script.js
@@ -4,5 +4,9 @@
 (() => {
 
     // your code here
+    items = document.getElementsByClassName("target");
+    for (let val in items) {
+        document.getElementsByClassName("target")[val].innerText = "owned ";
+    }
 
 })();
