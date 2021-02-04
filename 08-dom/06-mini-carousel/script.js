5654423e6b1b0050a5deefbfd9e91405248f0a61
zakaria <zakariaselassi@gmail.com>
2021-02-04 15:46:32 +0100

    carousel

--- 08-dom/06-mini-carousel/script.js
+++ 08-dom/06-mini-carousel/script.js
@@ -3,7 +3,7 @@
 
 (() => {
 
-    let gallery= [
+    let gallery = [
         "../../_shared/img/bell.svg",
         "../../_shared/img/clock.svg",
         "../../_shared/img/compass.svg",
@@ -11,6 +11,24 @@
         "../../_shared/img/map.svg",
     ];
 
+    let first = document.querySelector("img");
+    let src = first.src;
+    console.log(src);
+    let cpt = 0;
+
+    document.getElementById("next").addEventListener("click", function() {
+
+        if (cpt >= gallery.length - 1) {
+            cpt = 0;
+        }
+
+        first.src = gallery[cpt];
+        cpt = cpt + 1;
+
+    });
+
     // your code here
+
+
 
 })();
