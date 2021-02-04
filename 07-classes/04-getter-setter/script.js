aebbcbad17e0d0c11572ac1558f763d5c14bdfa8
zakaria <zakariaselassi@gmail.com>
2021-02-04 10:52:21 +0100

    getter setter

--- 07-classes/04-getter-setter/script.js
+++ 07-classes/04-getter-setter/script.js
@@ -3,4 +3,26 @@
 
 (() => {
     // your code here
+    class Person {
+        constructor(firstname, lastname) {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+        get Name() {
+            return this.firstname + " " + this.lastname;
+        }
+        set Name(values) {
+            this.firstname = values;
+            this.lastname = values;
+        }
+    }
+
+    document.getElementById("run").addEventListener("click", () => myFunction());
+
+    function myFunction() {
+        let myPerson = new Person("Pixels", "Art");
+
+        console.log(myPerson.Name);
+    }
+
 })();
