1308f01140a03cca35562227469838a158083f71
zakaria <zakariaselassi@gmail.com>
2021-02-04 13:35:30 +0100

    attr-create

--- 08-dom/04-attr-create/script.js
+++ 08-dom/04-attr-create/script.js
@@ -2,7 +2,17 @@
 
 
 (() => {
-
+    /**Get the value of the "data-image" attribute from the element with id "source". Create a new image element inside the target element that displays the image referred to by the attribute value. Then remove the original element. */
     // your code here
+
+    let first = document.getElementById("source");
+
+    let cop = first.dataset.image;
+    console.log(cop);
+    let myImage = document.createElement("IMG");
+    myImage.setAttribute("src", cop);
+    document.querySelector("#target").appendChild(myImage);
+
+
 
 })();
