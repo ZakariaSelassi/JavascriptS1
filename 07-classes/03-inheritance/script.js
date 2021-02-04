339ba0071105004cf8426ea4f84b450ac8baa158
zakaria <zakariaselassi@gmail.com>
2021-02-04 10:41:50 +0100

    inerithance

--- 07-classes/03-inheritance/script.js
+++ 07-classes/03-inheritance/script.js
@@ -7,5 +7,23 @@
             return `${this.constructor.greeting}! I'm ${this.name}!`;
         }
     }
+    class Cat extends Animal {
+        static greeting = "Miaou";
+        constructor(name) {
+            super(name);
+            this.name = name;
+        }
+    }
+    class Dog extends Animal {
+        static greeting = "Wouaf";
+        constructor(name) {
+            super(name);
+            this.name = name;
+        }
+    }
     // your code here
+    let myCat = new Cat("Pixels");
+    console.log(myCat.sayHello());
+    let myDog = new Dog("Gradiant");
+    console.log(myDog.sayHello());
 })();
