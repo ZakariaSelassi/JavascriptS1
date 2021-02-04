294fbce9092e5ee253c26282b924d9441cac6661
zakaria <zakariaselassi@gmail.com>
2021-02-04 20:28:46 +0100

    match

--- 08-dom/08-generate-table-two/script.js
+++ 08-dom/08-generate-table-two/script.js
@@ -3,5 +3,21 @@
 (() => {
 
     // your code here
+    let table = document.createElement("table");
+    table.setAttribute("id", "myTable");
+    document.getElementById("target").appendChild(table);
+
+    for (let i = 0; i < 10; i++) {
+        let row = document.createElement("tr");
+        document.getElementById("myTable").appendChild(row);
+
+        for (let j = 0; j < 10; j++) {
+            let cols = document.createElement("td");
+            let text = document.createTextNode((i + 1) * (j + 1));
+            cols.appendChild(text);
+            row.appendChild(cols);
+        }
+    }
+
 
 })();
--- 08-dom/09-match-password-one/script.js
+++ 08-dom/09-match-password-one/script.js
@@ -4,5 +4,26 @@
 (() => {
 
     // your code here
+    document.getElementById("run").addEventListener("click", () => myFunction());
+
+    function myFunction() {
+        // recup premiere valeur 
+        let firstValue = document.getElementById("pass-one").value;
+
+        let secondValue = document.getElementById("pass-two").value;
+
+
+        if (firstValue !== secondValue || firstValue == "" || secondValue == "") {
+            alert("Password aren't the same ! ");
+            document.querySelector("input").style.borderColor = "red";
+        } else {
+            console.log("Value are the same ! ");
+            console.log(" first " + firstValue + " " + "second " + secondValue);
+            document.querySelector("input").style.borderColor = "black";
+        }
+    }
+
+
+
 
 })();
