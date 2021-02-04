01f2ac7220744a16bc0aa2e1028f9a22e5b13a1a
zakaria <zakariaselassi@gmail.com>
2021-02-04 16:13:17 +0100

    table-one

--- 08-dom/07-generate-table-one/script.js
+++ 08-dom/07-generate-table-one/script.js
@@ -4,5 +4,15 @@
 (() => {
 
     // your code here
+    let table = document.createElement("table");
+    table.setAttribute("id", "myTable");
+    document.getElementById("target").appendChild(table);
+
+    for (let i = 0; i < 10; i++) {
+        let col = document.createElement("tr");
+        col.setAttribute("class", "myTr");
+        document.getElementById("myTable").appendChild(col);
+    }
+
 
 })();
