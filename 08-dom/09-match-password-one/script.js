c8126a75d720dadd4c6a52d74a56441796e3683c
zakaria <zakariaselassi@gmail.com>
2021-02-05 10:09:07 +0100

    change event

--- 08-dom/09-match-password-one/script.js
+++ 08-dom/09-match-password-one/script.js
@@ -11,16 +11,22 @@
         let firstValue = document.getElementById("pass-one").value;
 
         let secondValue = document.getElementById("pass-two").value;
-
+        console.log(document.querySelectorAll("input"));
+        let myVar = document.querySelectorAll("input");
 
         if (firstValue !== secondValue || firstValue == "" || secondValue == "") {
             alert("Password aren't the same ! ");
-            document.querySelector("input").style.borderColor = "red";
+            for (let i = 0; i < myVar.length; i++) {
+                myVar[i].style.borderColor = "red";
+            }
         } else {
             console.log("Value are the same ! ");
             console.log(" first " + firstValue + " " + "second " + secondValue);
-            document.querySelector("input").style.borderColor = "black";
+            for (let i = 0; i < myVar.length; i++) {
+                myVar[i].style.borderColor = "silver";
+            }
         }
+
     }
 
 
--- 08-dom/11-change-event-input-one/script.js
+++ 08-dom/11-change-event-input-one/script.js
@@ -4,5 +4,21 @@
 (() => {
 
     // your code here
+    document.getElementById("pass-one").addEventListener("input", myFunction);
+
+    function myFunction() {
+        let count = document.getElementById("pass-one").value.length;
+        let max_lengts = 10;
+        if (count <= max_lengts) {
+            document.getElementById("counter").innerText = count;
+        }
+
+        if (count > max_lengts) {
+            document.getElementById("pass-one").setAttribute("maxlength", max_lengts);
+        }
+
+    }
+
+
 
 })();
