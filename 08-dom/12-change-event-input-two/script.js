7e6c49f9a5f327456962b732f66cc2fbe1681538
zakaria <zakariaselassi@gmail.com>
2021-02-05 14:25:51 +0100

    change event

--- 08-dom/12-change-event-input-two/script.js
+++ 08-dom/12-change-event-input-two/script.js
@@ -4,5 +4,22 @@
 (() => {
 
     // your code here
+    document.getElementById("pass-one").addEventListener("change", myFunction);
 
+    function myFunction() {
+        let input = document.querySelector('input');
+        let target = document.getElementById('validity');
+
+        console.log(input.value);
+        var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+        if (input.value.match(passw)) {
+            alert('Correct, try another...');
+            return true;
+        } else {
+            alert('Wrong...!');
+            return false;
+        }
+
+
+    }
 })();
