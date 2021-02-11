c1bc746e271e7d227bf8373ec67616aaadcb3279
zakaria <zakariaselassi@gmail.com>
2021-02-11 11:54:25 +0100

    typewriter effect

--- 09-misc/02-typewriter-effect/script.js
+++ 09-misc/02-typewriter-effect/script.js
@@ -4,5 +4,23 @@
 (() => {
 
     // your code here
+    let text = document.getElementById("target");
+    console.log(text);
+    let stringT = text.textContent;
+    text.innerHTML = " ";
+    console.log(stringT);
+    let cpt = 0;
 
+
+    function typeWriter() {
+        let nb = Math.floor(Math.random() * (1000 - 1)) + 1;
+        console.log(nb);
+        if (cpt < stringT.length) {
+            document.getElementById("target").innerHTML += stringT.charAt(cpt); // chartAt : a chaque fois qu'un lettre est parcourur renvoie une nouvelle chaine containant le caractere
+            cpt++;
+            setTimeout(typeWriter, nb);
+        }
+    }
+
+    window.onload = typeWriter;
 })();
