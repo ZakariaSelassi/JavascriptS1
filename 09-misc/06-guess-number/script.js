023c9a31304198e8a04558ed7e0a7314660f9e0a
zakaria <zakariaselassi@gmail.com>
2021-02-12 15:06:42 +0100

    bon

--- 09-misc/06-guess-number/script.js
+++ 09-misc/06-guess-number/script.js
@@ -3,5 +3,23 @@
 (() => {
 
     // your code here
+    let x = Math.floor(Math.random() * (100 - 1)) + 1;
+    //alert(x);
+    let enter;
+    let cpt = 0;
+    while (enter != x) {
+        enter = prompt("Guess number ! ", "Enter correct number");
+        cpt++;
+        if (enter < x) {
+            alert("Higher");
+        } else if (enter > x) {
+            alert("lower");
+        } else if (enter == x) {
+            alert("That's good guess ! " + cpt);
+        }
+
+    }
+
+
 
 })();
